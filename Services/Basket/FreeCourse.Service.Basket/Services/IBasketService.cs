2397d3ccfcca3b02b97ff122f94c059f805b9959
unknown <oyurt43@gmail.com>
2023-02-26 17:46:36 +0300

    yeni

---
+++ Services/Basket/FreeCourse.Service.Basket/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Service.Basket.Dtos;
+using FreeCourseShared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Service.Basket.Services
+{
+   public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
